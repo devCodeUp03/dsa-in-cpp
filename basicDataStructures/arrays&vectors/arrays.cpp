1b934c99b09e470de7ab2e5f359a157d268fd514
Devashish Upreti <devashup68@gmail.com>
2025-08-26 21:17:22 +0545

    solved some problems in arrays from geeeksforgeeks

--- basicDataStructures/arrays&vectors/arrays.cpp
+++ basicDataStructures/arrays&vectors/arrays.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-
-int main() {
+int main()
+{
 
     int arr[5] = {1, 2, 3, 4, 5}; // declaring and initializing 1D array
-    cout<<arr[4]; //accessing 1D array
+    // cout<<arr[4]; //accessing 1D array
+    int n = sizeof(arr) / sizeof(arr[0]);
+    sort(arr, arr + n, greater<int>());
+    for (int el : arr)
+    {
+        cout << el << " ";
+    }
 }
--- basicDataStructures/arrays&vectors/vectors.cpp
+++ basicDataStructures/arrays&vectors/vectors.cpp
@@ -1,11 +1,12 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 void printVector(const vector<int> &v);
 
-
-int main() {
+int main()
+{
 
     // vector<int> v = {-10, 0};
     // v.push_back(10);
@@ -17,15 +18,19 @@ int main() {
     //     cout<<*it<<" ";
     // }
 
-    vector<int> v(5, 11);
-    v.push_back(20);
+    // vector<int> v(5, 11);
+    // v.push_back(20);
+    // printVector(v);
+    // cout<<"Hello, World"<<endl;
+
+    vector<int> v = {8, 9, 2, 1, 5, 4};
+    sort(v.begin(), v.end(), greater<int>());
     printVector(v);
-    cout<<"Hello, World"<<endl;
-
-
 }
 
-void printVector(const vector<int> &v) {
-    for(int el: v) cout<<el<<" ";
-    cout<<"\n";
+void printVector(const vector<int> &v)
+{
+    for (int el : v)
+        cout << el << " ";
+    cout << "\n";
 }
